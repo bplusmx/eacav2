e5120234c0f1b4253abe58d8094e15604e3251f3
Luis Abarca <luis.abarca@justoalblanco.com>
2014-07-31 20:35:57 -0500

    Scripts de slideck

--- js/jquery.actions.js
+++ js/jquery.actions.js
@@ -1,73 +1,60 @@
 var carPos = 1;
 var detenido = false;
 
-function mycarousel_initCallback(carousel) 
+(function($)
 {
-	$('#features-nav .features-nav-item').click(function() 
-	{
-		//carPos = jQuery.jcarousel.intval(jQuery(this).attr("rel"));
-		
-		//alert(carPos);
-		
-		//carousel.scroll(carPos);		
-		//detenido = true;
-		//$("#features-nav .features-nav-item").removeClass("current").addClass("");
-	    //$(this).addClass("current");
-	    
-		//return false;
-	});
-	
-	$("#features-nav .features-nav-item").mouseover(function()
-	{
-		//carousel.stopAuto();
-	});
-	
-	$("#features-nav .features-nav-item.current").mouseout(function()
-	{
-		//carousel.startAuto();
-	});
-	
-	// Pause autoscrolling if the user moves with the cursor over the clip.
-	/*
-    carousel.clip.hover(function() {
-        carousel.stopAuto();
-    }, function() {
-        carousel.startAuto();
+    jQuery(document).ready(function() 
+    {
+        $("#destacados").jcarousel({
+            scroll: 1,
+            auto: 3,
+            wrap: "last",
+            initCallback: mycarousel_initCallback,
+            itemLoadCallback: onLoadItem,
+            buttonNextHTML: null,
+            buttonPrevHTML: null
+        });
     });
-    */
+    
+    function mycarousel_initCallback(carousel) 
+    {
+        $('#features-nav .features-nav-item').click(function() 
+        {		
+        });
 
-};
+        $("#features-nav .features-nav-item").mouseover(function()
+        {
+            //carousel.stopAuto();
+        });
 
-function onLoadItem(carousel)
-{
-	if (!detenido) {
-		$("#features-nav .features-nav-item").removeClass("current").addClass("");
-		$("#features-nav #features-nav" + carPos).addClass("current");
-		
-		if (carPos < carousel.size()) {
-			carPos++;
-		} else if (carPos == carousel.size()) {
-			carPos = 1;
-		}
-	}
-	
-	//alert(carPos + " " + carousel.size());
-}
+        $("#features-nav .features-nav-item.current").mouseout(function()
+        {
+            //carousel.startAuto();
+        });
 
-jQuery(document).ready(function() 
-{
-	$("#destacados").jcarousel({
-		scroll: 1,
-		auto: 3,
-		wrap: "last",
-		initCallback: mycarousel_initCallback,
-		itemLoadCallback: onLoadItem,
-		buttonNextHTML: null,
-		buttonPrevHTML: null
-	});
+        // Pause autoscrolling if the user moves with the cursor over the clip.
+        /*
+        carousel.clip.hover(function() {
+            carousel.stopAuto();
+        }, function() {
+            carousel.startAuto();
+        });
+        */
 
-	$("#features-nav .features-nav-item").click(function()
-	{		
-	    
-	});
-});
+    };
+
+    function onLoadItem(carousel)
+    {
+        if (!detenido) {
+            $("#features-nav .features-nav-item").removeClass("current").addClass("");
+            $("#features-nav #features-nav" + carPos).addClass("current");
+
+            if (carPos < carousel.size()) {
+                carPos++;
+            } else if (carPos == carousel.size()) {
+                carPos = 1;
+            }
+        }
+    }
+})(jQuery);
+
