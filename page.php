0d539615236039c2ac2b4e14b000c1aae23c4d48
Luis Abarca <luis.abarca@justoalblanco.com>
2014-07-31 20:02:39 -0500

    Agregados estilos de bootstrap

--- page.php
+++ page.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<div class="post-page" id="post-<?php the_ID(); ?>">
-<h1><?php the_title(); ?></h1>
-<?php the_content('<p class="serif">Seguir leyendo &raquo;</p>'); ?>
-<?php wp_link_pages(array('before' => '<p><strong>Paginas:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+<div class="container">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="post-page" id="post-<?php the_ID(); ?>">
+    <h1><?php the_title(); ?></h1>
+    <?php the_content('<p class="serif">Seguir leyendo &raquo;</p>'); ?>
+    <?php wp_link_pages(array('before' => '<p><strong>Paginas:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+    </div>
+    <?php endwhile; endif; ?>
+    <?php edit_post_link('Editar nota.', '<p>', '</p>'); ?>
 </div>
-<?php endwhile; endif; ?>
-<?php edit_post_link('Editar nota.', '<p>', '</p>'); ?>
 
 <?php get_footer(); ?>
