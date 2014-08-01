0ae492018b4bc1dfd2b54dcfa085261e201127d6
Luis Abarca <luis.abarca@justoalblanco.com>
2014-07-31 20:03:06 -0500

    Estilos de bootstrap y sidebars

--- footer.php
+++ footer.php
@@ -1,72 +1,76 @@
+
+<div class="container">
+    <div class="col-sm-4">
+        <?php dynamic_sidebar('before-footer-1-3') ?>
     </div>
-
+    <div class="col-sm-4">
+        <?php dynamic_sidebar('before-footer-2-3') ?>
+    </div>
+    <div class="col-sm-4">
+        <?php dynamic_sidebar('before-footer-3-3') ?>
+    </div>
 </div>
 
-<?php if (!is_category(5) && !is_category(10)): ?>
-<div class="s1">
- <?php include TEMPLATEPATH . '/sidebar-2.php' ?>
-</div>
-<?php endif; ?>
-</div>
-
-<!-- End Con -->
-<div style="background-color: #fff; " class="con">
-	<div style="margin: 5px;">
-		<h3>Temas populares</h3>
-		<?php wp_tag_cloud(''); ?>
-	</div>
-</div>
-
-
-<div id="pie">
-<ul>
-<?php  wp_list_pages('title_li=&child_of=1680&exclude=2'); ?>
-<li><a href="/publicidad">Publicidad</a></li>
-</ul>
-
-    <div>
-		e-Acapulco.com &reg; 2001-<?php echo date('Y') ?> | Comentarios y Sugerencias a <a href="mailto:info@e-acapulco.com">info@e-acapulco.com</a>
-		<br />Acapulco, Guerrero
-	</div>
+<div class="container">
+    <div class="col-sm-3">
+        <?php dynamic_sidebar('before-footer-1-4') ?>
+    </div>
+    <div class="col-sm-3">
+        <?php dynamic_sidebar('before-footer-2-4') ?>
+    </div>
+    <div class="col-sm-3">
+        <?php dynamic_sidebar('before-footer-3-4') ?>
+    </div>
+    <div class="col-sm-3">
+        <?php dynamic_sidebar('before-footer-4-4') ?>
+    </div>
 </div>
 
-<div id="close">
-	<div>&nbsp;</div>
-	<p><br />Powered By: <a style="color: #878; font-size: 90%" href="http://justoalblanco.com/?utm_source=clients&utm_medium=banner&utm_term=e-acapulco&utm_content=footertext&utm_campaign=branding">Justo al Blanco</a></p>
-</div>
+</div> <!-- /.page -->
 
-</div>
+<footer>
+    <div class="container">
+        <div class="col-sm-4">
+            <?php dynamic_sidebar('footer-1-3') ?>
+        </div>
+        <div class="col-sm-4">
+            <?php dynamic_sidebar('footer-2-3') ?>
+        </div>
+        <div class="col-sm-4">
+            <?php dynamic_sidebar('footer-3-3') ?>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="col-sm-3">
+            <?php get_template_part('partials/newsletter') ?>
+        </div>
+        <div class="col-sm-3">
+            <?php get_template_part('partials/facebook-likebox') ?>
+        </div>
+        <div class="col-sm-6">
+            <?php get_template_part('partials/destinos') ?>
+        </div>
+    </div>
+    
+    <div class="container">
+        <ul class="nav nav-pills">
+        <?php  wp_list_pages('title_li=&child_of=1680&exclude=2'); ?>
+        </ul>
 
-<!-- End BG -->
+        <div>
+            e-Acapulco.com &reg; 2001-<?php echo date('Y') ?> | Comentarios y Sugerencias a <a href="mailto:info@e-acapulco.com">info@e-acapulco.com</a>
+            <br />Acapulco, Guerrero
+        </div>
+
+
+        <p><br />Powered By: <a style="color: #878; font-size: 90%" href="http://justoalblanco.com/?utm_source=clients&utm_medium=banner&utm_term=e-acapulco&utm_content=footertext&utm_campaign=branding">Justo al Blanco</a></p>
+    </div>
+</footer>
+
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory') ?>/js/jquery.jcarousel.js"></script>
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory') ?>/js/jquery.actions.js"></script>
+
 <?php wp_footer(); ?>
-<script type="text/javascript">
-jQuery(document).ready(function()
-{
-    jQuery(".top-title").click(function()
-    {
-        window.location = "http://e-acapulco.com/";
-    }).css('cursor', 'pointer');
-
-    /*
-    $('.vans,.yates,.mapa').css('cursor', 'pointer');
-
-    $('.vans,.yates,.mapa').click(function()
-    {
-        clase = $(this).attr('class');
-        url = $('.' + clase + ' a').attr('href');
-        window.location = url;
-    });
-    */
-});
-</script>
-
-
-<script type="text/javascript">
-setTimeout(function(){var a=document.createElement("script");
-var b=document.getElementsByTagName("script")[0];
-a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0017/9761.js?"+Math.floor(new Date().getTime()/3600000);
-a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
-</script>
-
 </body>
 </html>
